12922cd5a369c04f8fa1dab50b7ba154813de006
dong <1900756736@qq.com>
2019-06-06 23:20:33 +0800

    添加redis

--- src/main/java/com/xiaodongbaobao/novel/api/NContentDetailApi.java
+++ src/main/java/com/xiaodongbaobao/novel/api/NContentDetailApi.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import com.baidu.aip.nlp.AipNlp;
@@ -17,43 +20,57 @@ import com.xiaodongbaobao.novel.bean.CosProcess;
 import com.xiaodongbaobao.novel.entity.NNovelContent;
 import com.xiaodongbaobao.novel.service.INNovelContentService;
 import com.xiaodongbaobao.novel.utils.FanYiUtil;
+import com.xiaodongbaobao.novel.utils.RedisUtil;
+
 @Service("nContentDetailApi")
-public class NContentDetailApi implements IApiProcessService{
+public class NContentDetailApi implements IApiProcessService {
 	@Autowired
 	private INNovelContentService iNNovelContentService;
 	@Autowired
 	private CosProcess cosProcess;
 	@Autowired
 	private AipNlp client;
+	@Autowired
+	private RedisUtil redisUtil;
+
 	@Override
-	public Map<String, Object> apiProcess(ParamContextHolder paramContextHolder, CApiBase cApiBase) throws ApiProcessException {
+	public Map<String, Object> apiProcess(ParamContextHolder paramContextHolder, CApiBase cApiBase)
+			throws ApiProcessException {
 		String id = paramContextHolder.getReq().getParameter("id");
+		String language = paramContextHolder.getReq().getParameter("language");
+		String str = null;
 		NNovelContent content = iNNovelContentService.selectById(id);
-		if(content == null ) throw new ApiProcessException("C0001", "没有该书籍");
+		if (content == null)
+			throw new ApiProcessException("C0001", "没有该书籍");
 		Map<String, Object> map = new HashMap<>();
 		ArrayList<Map<String, Object>> list = iNNovelContentService.selectByContent(content);
-		Map<String, Object> result = list.get(0);
-		String str = cosProcess.getContent(content.getBookId()+"/"+content.getId());
-		str = str.replaceAll("< br >|</br>|<br>", "");
-		String language = paramContextHolder.getReq().getParameter("language");
-		if(language.equals("zy")){
-			int maxSummaryLen = 300;
-			HashMap<String, Object> options = new HashMap<String, Object>();
-			JSONObject res = client.newsSummary(str, maxSummaryLen, options);
-			try{
-				result.put("content", "&emsp;&emsp;"+res.get("summary"));
-				map.put("result", result);
-			}catch(Exception e){
-				throw new ApiProcessException("c0001","内容摘要获取异常");
+		Map<String, Object> result = (Map)redisUtil.hmget(id + "_" + language);
+		if (result.size() == 0) {
+			result = list.get(0);
+			str = cosProcess.getContent(content.getBookId() + "/" + content.getId());
+			str = str.replaceAll("< br >|</br>|<br>", "");
+
+			if (language.equals("zy")) {
+				int maxSummaryLen = 300;
+				HashMap<String, Object> options = new HashMap<String, Object>();
+				JSONObject res = client.newsSummary(str, maxSummaryLen, options);
+				try {
+					result.put("content", "&emsp;&emsp;" + res.get("summary"));
+					map.put("result", result);
+				} catch (Exception e) {
+					throw new ApiProcessException("c0001", "内容摘要获取异常");
+				}
+				return map;
 			}
-			return map;
+			str = str.replaceAll("      ", "<br>");
+			if (!language.equals("zh")) {
+				str = FanYiUtil.translate(str, language);
+			}
+			str = str.replaceAll("< br >|</br>|<br>", "<br>&emsp;&emsp;");
+			result.put("content", str);
+			redisUtil.hmset(id + "_" + language, result);
 		}
-		str = str.replaceAll("      ", "<br>");
-		if(!language.equals("zh")){
-			str = FanYiUtil.translate(str, language);
-		}
-		str = str.replaceAll("< br >|</br>|<br>", "<br>&emsp;&emsp;");
-		result.put("content",str);
+
 		map.put("result", result);
 		return map;
 	}
